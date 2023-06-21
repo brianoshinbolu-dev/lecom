e2e18523bde24f2434cc25cce91a1fb016b06577
Brian <oshinbolubrian@gmai;.com>
2023-06-21 12:29:33 +0100

    Register Page done

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -3,6 +3,7 @@ import styles from "./auth.module.scss";
 import loginImg from "../../assets/login.png";
 import { Link } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
+import Card from "../../components/card/Card";
 
 const Login = () => {
   return (
@@ -11,26 +12,29 @@ const Login = () => {
         <img src={loginImg} alt="login" width="400" />
       </div>
 
-      <div className={styles.form}>
-        <h2>Login</h2>
-        <form>
-          <input type="text" placeholder="Email" required />
-          <input type="password" placeholder="Password" required />
-          <button className="--btn --btn-primary --btn-block">Login</button>
-          <div className={styles.links}>
-            <Link to="/reset">Reset password</Link>
-          </div>
-          <p>-- or --</p>
-        </form>
-        <button className="--btn --btn-danger --btn-block">
-          <FaGoogle />
-          Login With Google
-        </button>
-        <span className={styles.register}>
-          <p>Dont have an Account?</p>
-          <Link to="/register">Register</Link>
-        </span>
-      </div>
+      <Card>
+        <div className={styles.form}>
+          <h2>Login</h2>
+
+          <form>
+            <input type="text" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            <button className="--btn --btn-primary --btn-block">Login</button>
+            <div className={styles.links}>
+              <Link to="/reset">Reset password</Link>
+            </div>
+            <p>-- or --</p>
+          </form>
+          <button className="--btn --btn-danger --btn-block">
+            <FaGoogle />
+            Login With Google
+          </button>
+          <span className={styles.register}>
+            <p>Dont have an Account?</p>
+            <Link to="/register"><strong>Register</strong></Link>
+          </span>
+        </div>
+      </Card>
     </section>
   );
 };
